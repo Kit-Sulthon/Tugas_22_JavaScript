d4aab2d34f31ecbb0c33bc80637d9dd1fd6799ce
T R A $ H <sulthons.a19@gmail.com>
2023-10-05 16:19:47 +0700

    Tugas 22 JavaScript
    Tugas ini kami kumpulkan untuk Niomic

---
+++ tugas22.js
@@ -0,0 +1,7 @@
+function Tugas22() {
+  var array1 = ["Saya", "ingin", "belajar", "bersama"];
+  array1.forEach(function (item, index) {
+    console.log("item : ", item, " index ke ", index);
+  });
+}
+Tugas22();
